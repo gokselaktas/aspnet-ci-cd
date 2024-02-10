42ed32485255af152c2c840a6b1093ae733eca13
Goksel Aktas <aktas_goksel@hotmail.com>
2024-02-10 14:06:58 +0000

    added docker, docker-compose and start.sh ci/cd stuff

---
+++ Controllers/TestController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ansible.Controllers;
+
+[ApiController]
+[Route("/test")]
+public class TestController : ControllerBase
+{
+    [HttpGet]
+    public string Get()
+    {
+        return "gokselaktas";
+    }
+}
